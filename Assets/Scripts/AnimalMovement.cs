44392123c37653ac1efeda7f69c061dc346d9c24
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-04-18 14:03:53 +0300

    Refactoring of AnimalMovement script

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -1,42 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AnimalPOV
 {
-    public class NewBehaviourScript : MonoBehaviour
+    public interface IMovementParameters
     {
-        public interface IControl
+        float MovementSpeed
         {
+            get;
+        }
+        float RotationSpeed
+        {
+            get;
+        }
+        float JumpForce
+        {
+            get;
 
         }
-       public class Rat : IControl
+    }
+    [Serializable()] 
+    public class Rat : IMovementParameters
         {
-            private float movementSpeed = 5f;
-            public float MovementSpeed
-            {
-                get
-                {
-                    return movementSpeed;
-                }
-            }
-            private float rotationSpeed = 90f;
-            public float RotationSpeed
-            {
-                get
-                {
-                    return rotationSpeed;
-                }
-            }
-            private float jumpForce = 10f;
-            public float JumpForce
-            {
-                get
-                {
-                    return jumpForce;
-                }
-            }
-        }
+        [SerializeField] 
+        private float movementSpeed = 5f;
+            public float MovementSpeed => movementSpeed;
+        [SerializeField]
+        private float rotationSpeed = 90f;
+            public float RotationSpeed => rotationSpeed;
+        [SerializeField]
+        private float jumpForce = 10f;
+            public float JumpForce => jumpForce;
 
+        }
     }
 }
