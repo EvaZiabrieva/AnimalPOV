11426aa3f1088eb1a0b8d5a0112f6c2eaf62dfe1
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-04-18 21:25:26 +0300

    Fix wrongs in script

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -3,37 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AnimalPOV
+namespace MovementParameters
 {
     public interface IMovementParameters
     {
-        float MovementSpeed
-        {
-            get;
-        }
-        float RotationSpeed
-        {
-            get;
-        }
-        float JumpForce
-        {
-            get;
-
-        }
+        float MovementSpeed { get; }
+        float RotationSpeed { get; }
+        float JumpForce { get; }
     }
-    [Serializable()] 
-    public class Rat : IMovementParameters
-        {
-        [SerializeField] 
+    [Serializable]
+    public class MovementParameters : IMovementParameters
+    {
+        [SerializeField]
         private float movementSpeed = 5f;
-            public float MovementSpeed => movementSpeed;
+        public float MovementSpeed => movementSpeed;
         [SerializeField]
         private float rotationSpeed = 90f;
-            public float RotationSpeed => rotationSpeed;
+        public float RotationSpeed => rotationSpeed;
         [SerializeField]
         private float jumpForce = 10f;
-            public float JumpForce => jumpForce;
-
-        }
+        public float JumpForce => jumpForce;
     }
 }
