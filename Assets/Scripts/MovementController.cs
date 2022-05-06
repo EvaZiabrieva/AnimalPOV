710ebc3212cd31dee60df00fc0aae1f532d3ceb0
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-05-06 15:14:14 +0300

    create target rotation

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -13,18 +13,21 @@ namespace AnimalPOV
         {
             RaycastHit hit;
             Ray ray = new Ray(transform.position + Vector3.up * 0.05f, Vector3.down);
+            Quaternion targetRotation;
             if (Physics.Raycast(ray, out hit, 0.1f, raycastMask))
-                transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                targetRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
             else
-                transform.rotation = Quaternion.identity;
+                targetRotation = Quaternion.identity;
+            
 
             float angle = Vector3.Angle(Vector3.up, hit.normal);
             float speedModificator = Mathf.InverseLerp(movementParameters.MaxAngle, 0, angle);
             float horizontalMovement = inputProvider.GetForwardMovement() * movementParameters.MovementSpeed * speedModificator * Time.deltaTime;
 
             horizontalRotation += inputProvider.GetRotation() * movementParameters.RotationSpeed * Time.deltaTime;
-            transform.rotation *= Quaternion.Euler(0, horizontalRotation, 0);
-            transform.position += transform.rotation * new Vector3(horizontalMovement, 0f, 0f);
+            targetRotation *= Quaternion.Euler(0, horizontalRotation, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+            transform.position += targetRotation * new Vector3(horizontalMovement, 0f, 0f);
         }
     }
 }
